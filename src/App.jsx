24eb1761c774b5a660f06e915dfa73ee6d2aad45
Kelian Halleray <kelianhalleray@gmail.com>
2023-12-13 16:27:08 +0100

    First Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,22 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/data.json');
+        const response = await fetch("./data.json");
+  
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+  
         const data = await response.json();
         setArticles(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
+  
     fetchData();
   }, []);
+  
 
   return (
     <Router>
