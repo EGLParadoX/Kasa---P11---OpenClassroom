0116dcfe46fe3a4f6281955333adaa115b480209
Kelian Halleray <kelianhalleray@gmail.com>
2023-12-13 16:05:18 +0100

    First Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://eglparadox.github.io/Kasa---P11---OpenClassroom/',
+  base: 'https://eglparadox.github.io/Kasa---P11---OpenClassroom',
   plugins: [react()],
 })
